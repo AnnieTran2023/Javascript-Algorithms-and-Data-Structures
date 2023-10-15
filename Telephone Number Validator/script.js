b260a0d6dc727acb0119dfca389626e67487d520
Annie Tran <132701766+AnnieTran2023@users.noreply.github.com>
2023-10-15 17:52:46 +0300

    Create script.js

---
+++ Telephone Number Validator/script.js
@@ -0,0 +1,22 @@
+/*
+JavaScript Algorithms and Data Structures Projects: Telephone Number Validator
+Return true if the passed string looks like a valid US phone number.
+
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+*/
+
+function telephoneCheck(str) {
+  //removing all non-digit characters from the string 
+  const transformedStr = str.replace(/[^\d]/g,'');
+  //check string length
+  
+}
+
+telephoneCheck("555-555-5555");
